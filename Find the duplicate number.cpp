2dc9946289fefa6da8ba2649bae253a6e641d020
Anmol Garg <80644106+Scholasticpal@users.noreply.github.com>
2022-05-10 23:55:14 +0530

    Create Find the duplicate number.cpp

---
+++ Find the duplicate number.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+class Solution {
+public:
+     int findDuplicate(vector<int>& nums) {
+         int rep = 0;
+        sort(nums.begin(),nums.end());
+         
+        for(int i = 0; i < nums.size() - 1; i++)
+        {
+            if(nums[i] == nums[i+1])
+            {
+                rep = nums[i];
+                break;
+            }
+        }
+        return rep;
+     }
+};
