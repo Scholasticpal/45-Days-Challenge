c8638cb7f78b5c7611dd11559b4865c800082ef6
Anmol Garg <80644106+Scholasticpal@users.noreply.github.com>
2022-06-27 01:47:52 +0530

    Update maximum-product-of-three-numbers.cpp

--- maximum-product-of-three-numbers.cpp
+++ maximum-product-of-three-numbers.cpp
@@ -1,1 +1,16 @@
+// https://leetcode.com/problems/maximum-product-of-three-numbers/submissions/
 
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        return max((nums[n-1] * nums[n-2] * nums[n-3]), (nums[n-1] * nums[0] * nums[1]));
+    }
+};
+
+/* Functions used:
+- nums.size();
+- nums.begin();
+- nums.end();
+*/
