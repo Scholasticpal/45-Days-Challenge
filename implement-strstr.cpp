246653f587fb56eebb5a1e64542c1ca5a68c0557
Anmol Garg <80644106+Scholasticpal@users.noreply.github.com>
2022-06-27 10:03:36 +0530

    Update implement-strstr.cpp

--- implement-strstr.cpp
+++ implement-strstr.cpp
@@ -3,17 +3,19 @@ public:
     int strStr(string haystack, string needle) {
         int m = needle.length();
         int n = haystack.length();
-        int p = 0;
-        int t = 0;
-        int d = 5;
+        int p = 0, t=0, d= 256, val = 0;
         int count = 0;
-        if(m ==0){
-            return -1;
-            break;
+        int h = 1;
+        const int q=101;
+        if(m == 0){
+            return 0;
+        }
+        for(int i = 1; i<m; i++){
+            h = (h*d)%q;
         }
         for(int i = 0; i<m; i++){
-            p = int(needle[i]) + p*d;
-            t = int(haystack[i]) + t*d;
+            p = (int(needle[i]) + p*d)%q;
+            t = (int(haystack[i]) + t*d)%q;
         }
         for(int i = 0; i<n; i++){
             if(p==t){
@@ -23,14 +25,23 @@ public:
                         flag = false;
                         break;
                     }
-                }
-                if(flag==true){
-                    count +=1;
+                    if(flag==true){
+                     val = i;
+                     return val;
+                    }
                 }
             }
             if(i< n-m){
-                t = d*(t-int)
+                t = (d*(t-h*int(haystack[i])) + haystack[i+m])%q;
+                if(t<0)t=t+q; // the following line fails the code for test case: "mississippi" "sipp". will write a better code for rabi karp.
+                
             }
+        }
+        if(val == 0){
+            return -1;
+        }
+        else{
+            return val;
         }
     }
 
